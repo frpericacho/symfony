aa675de901bce1924b4fcc4b72696128138e7d94
frpericacho <frpericacho@gmail.com>
2020-04-26 12:22:14 +0200

    working on User

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
+/**
+ * @Route("/usuarios")
+ */
 class UsuariosController extends AbstractController
 {
     /**
-     * @Route("/usuarios", name="usuarios")
+     * @Route("/", name="registro_usuario")
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        //Paso1:
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        //Paso2
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //Paso3
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPlainPassword($password);
+
+            //Paso4
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage', array('user'=>$user));
+        }
         return $this->render('usuarios/index.html.twig', [
-            'controller_name' => 'UsuariosController',
+            'form' => $form->createView()
         ]);
     }
 }
